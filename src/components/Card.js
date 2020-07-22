2de0c1de2214fc8140d2f6a664a57f79bc9f41b6
Jyoti <jyoti.contact@gmail.com>
2020-07-22 16:46:11 +0100

    cors-anywhere workaround with axios

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Track from './Track';
 
 function Card() {
     const [music, setMusic] = useState([]);
 
     useEffect(() => {
-        // blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
-        const url = 'https://openwhyd.org/hot/electro?format=json';
-
-        fetch(url).then((response) => {
-            return response.json()
-        }).then((data) => {
-
-            setMusic(data.tracks);
+        // TODO this cors-anywhere url is a workaround for dev stage MVP only
+        const url = 'https://cors-anywhere.herokuapp.com/https://openwhyd.org/hot/electro?format=json';
+        axios.get(url).then((response) => {
+            console.log(response.data);
+            return response.data;
         })
-            .catch((error) => console.error(error));
+            .then((data) => {
+                setMusic(data.tracks);
+            })
+            .catch((error) => {
+                console.error("Failed to fetch hot electro tracks", error)
+            });
     }, [])
-
 
     const renderItems = () =>
         music.map((item) => <Track key={item._id} item={item} />)
