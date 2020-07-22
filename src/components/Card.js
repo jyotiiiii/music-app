9b0ed6a52bdc8b91ffeb877848a35a5d59673af4
Jyoti <jyoti.contact@gmail.com>
2020-07-22 16:09:23 +0100

    add Track component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Track from './Track';
 
 function Card() {
---
+++ src/components/Track.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Track = ({ item }) => (
+    <li>
+        <p>{item.name}</p>
+    </li>
+);
+
+export default Track;
