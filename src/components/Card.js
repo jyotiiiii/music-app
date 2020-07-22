c85ec87c66165b109a01d71d4ef01b15cb343eec
Jyoti <jyoti.contact@gmail.com>
2020-07-22 16:03:40 +0100

    add map to renderItems tracks in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Track from './Track';
 
 function Card() {
+    const [music, setMusic] = useState([]);
+
+    const renderItems = () =>
+        music.map((item) => <Track key={item._id} item={item} />)
     return (
         <ul>
             {renderItems()}
