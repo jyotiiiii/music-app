6191f25f257c387458bc11d23754c40297635a97
Jyoti <jyoti.contact@gmail.com>
2020-07-22 16:22:37 +0100

    add useEffect hook to handle fetch

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Track from './Track';
 
 function Card() {
     const [music, setMusic] = useState([]);
+
+    useEffect(() => {
+        // blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+        const url = 'https://openwhyd.org/hot/electro?format=json';
+
+        fetch(url).then((response) => {
+            return response.json()
+        }).then((data) => {
+
+            setMusic(data.tracks);
+        })
+            .catch((error) => console.error(error));
+    }, [])
+
 
     const renderItems = () =>
         music.map((item) => <Track key={item._id} item={item} />)
